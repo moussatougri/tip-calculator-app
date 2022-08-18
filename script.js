4d4595d956b7b3f8545567d29a1e46d06b35d01a
moussatougri <moussatougri23@gmail.com>
2022-08-18 12:57:46 +0200

    form validaton

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //selektoren
 const tipForm = document.querySelector(".form");
+const numberOfPeopleInp = document.querySelector(".number-people");
+const billInp = document.querySelector(".bill");
+const numberGroup = document.querySelector(".number-group");
+const billGroup = document.querySelector(".bill-group");
 
 //state
 const state = {
@@ -8,14 +12,32 @@ const state = {
 
 //press enter
 tipForm.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    const array = Array.from(tipForm).reduce(
-      (acc, input) => ({ ...acc, [input.name]: input.value }),
-      {}
-    );
+  //form validation
+  if (event.key === "Enter")
+    if (numberOfPeopleInp.value === "0" || billInp.value === "0") {
+      //validaton on number of People section
+      numberOfPeopleInp.style.border = "1px solid #ff0000";
+      const label = document.createElement("label");
+      label.for = "number-people";
+      label.classList.add("valid-label");
+      label.innerText = "Can`t be zero";
+      numberGroup.insertBefore(label, numberGroup.children[1]);
 
-    state.items.push(array);
+      //validaton on bill section
+      billInp.style.border = "1px solid #ff0000";
+      const billLabel = document.createElement("label");
+      billLabel.for = "bill";
+      billLabel.classList.add("bill-label");
+      billLabel.innerText = "Can`t be zero";
+      billGroup.insertBefore(billLabel, billGroup.children[1]);
+    } else {
+      event.preventDefault();
+      const array = Array.from(tipForm).reduce(
+        (acc, input) => ({ ...acc, [input.name]: input.value }),
+        {}
+      );
+      state.items.push(array);
+    }
 
-    console.log(state);
-  }
+  console.log(state);
 });
